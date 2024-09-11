7595d139864ec6be7e9661ca83dcf3d88201f216
Rafa MZ <mz.tears.of.time@gmail.com>
2024-09-11 11:38:36 -0300

    3. Process.argv

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// node .\index.js arquivos/texto-web.txt
+
+const caminho_Arquivo = process.argv;
+const link = caminho_Arquivo [ 2 ];
+
+console.log ( caminho_Arquivo );
+console.log ( `---\n` );
+console.log ( link );
