0c63827164242834d4589c4da359949430190ba3
Rafa MZ <mz.tears.of.time@gmail.com>
2024-09-12 16:06:07 -0300

    4. Ler arquivos

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Biblioteca
+# Documentação Node.js
+
+- [process.argv](https://nodejs.org/api/process.html#processargv)
+- [fs.readFile(path[, options], callback)](https://nodejs.org/api/fs.html#fsreadfilepath-options-callback)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-// node .\index.js arquivos/texto-web.txt
+const fs = require ( 'fs' ); // Importa o File System
+
+// node .\index.js ../arquivos/texto-web.txt
 
 const caminho_Arquivo = process.argv;
 const link = caminho_Arquivo [ 2 ];
 
-console.log ( caminho_Arquivo );
-console.log ( `---\n` );
-console.log ( link );
+/*
+	Dois parâmetros:
+		Caminho do arquivo;
+		Função callback que precisa receber dois parâmetros 
+*/
+fs.readFile ( link, 'utf-8', ( erro, texto ) => 
+{
+	console.log ( texto )
+})
+
+// console.log ( caminho_Arquivo );
+// console.log ( `---\n` );
+// console.log ( link );
