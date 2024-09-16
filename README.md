c3e62528e9b46cad896f53a4fbea107f88c453e7
Rafa MZ <mz.tears.of.time@gmail.com>
2024-09-16 16:06:41 -0300

    8. Add: método flatMap

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - [process.argv](https://nodejs.org/api/process.html#processargv)
 - [fs.readFile(path[, options], callback)](https://nodejs.org/api/fs.html#fsreadfilepath-options-callback)
+- [flatMap](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/flatMap)
--- src/index.js
+++ src/index.js
@@ -20,11 +20,13 @@ function Quebrar_Em_Paragrafos ( texto )
 	// Converte todo o texto em letras minúsculas e adiciona cada parágrafo em um elemento do array.
 	const paragrafos = texto.toLowerCase().split ( '\n' );
 
-	const contagem  = paragrafos.map (( paragrafo ) =>
+	const contagem  = paragrafos.flatMap (( paragrafo ) =>
 	{
+		if ( !paragrafo )
+			return [];
+
 		return Verificar_Palavras_Duplicadas ( paragrafo );
 	})
-
 	console.log ( contagem );
 }
 
@@ -50,3 +52,17 @@ function Verificar_Palavras_Duplicadas ( texto )
 
 	return resultado;
 }
+
+
+
+/*
+	Como transformamos esse filter e map em um único loop? Vamos usar outro método de array que ainda não conhecemos, o flatMap(), que também é um método callback.
+
+	Assim, const contagem será igual a paragrafos.flatMap(), que vai receber por parâmetro paragrafo seguido de arrow function.
+
+	Dentro das chaves, podemos fazer uma verificação if (!paragrafo), ou seja, se paragrafo for avaliado como falso, podemos retornar um array vazio. Isto é, return [].
+
+	Se não for um parágrafo vazio, se tiver conteúdo, vamos pegar o return verificaPalavrasDuplicadas(paragrafo) e colocá-lo dentro do flatMap().
+
+	Podemos deletar o filter e o map dessa função e deixar no final apenas o console.log(contagem).
+*/
