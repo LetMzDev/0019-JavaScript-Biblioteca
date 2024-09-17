c7db74b98325954dd12f19c1a035b58ec72262c4
Rafa MZ <mz.tears.of.time@gmail.com>
2024-09-17 16:19:49 -0300

    15. Salvando arquivo utilizando 'then'

--- src/cli.js
+++ src/cli.js
@@ -25,18 +25,39 @@ fs.readFile ( link, 'utf-8', ( erro, texto ) =>
 	}
 })
 
-async function Criar_Salvar_Arquivo ( lista_palavras, endereco )
+// async function Criar_Salvar_Arquivo ( lista_palavras, endereco )
+// {
+// 	const novo_Arquivo = `${endereco}/resultado.txt`;
+// 	const texto_Palavras = JSON.stringify ( lista_palavras );
+
+// 	try
+// 	{
+// 		await fs.promises.writeFile ( novo_Arquivo, texto_Palavras );
+// 		console.log ( "Arquivo criado" );
+// 	}
+// 	catch ( erro )
+// 	{
+// 		throw erro;
+// 	}
+// }
+
+function Criar_Salvar_Arquivo ( lista_palavras, endereco )
 {
 	const novo_Arquivo = `${endereco}/resultado.txt`;
 	const texto_Palavras = JSON.stringify ( lista_palavras );
 
-	try
-	{
-		await fs.promises.writeFile ( novo_Arquivo, texto_Palavras );
-		console.log ( "Arquivo criado" );
-	}
-	catch ( erro )
-	{
-		throw erro;
-	}
+	fs.promises.writeFile ( novo_Arquivo, texto_Palavras )
+		.then (() =>
+		{
+			// Processamento feito com o resultado da promessa
+			console.log ( "arquivo criado" );
+		})
+		.catch (( erro ) =>
+		{
+			throw erro;
+		})
+		.finally (() =>
+		{
+			console.log ( "Operação finalizada" );
+		})
 }
